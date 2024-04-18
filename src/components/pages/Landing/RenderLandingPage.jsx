455b418eb7cca97ff14dfe7d8ebfb1fde59f3c39
Christian Anderson <tehninjawaffle@gmail.com>
2024-04-17 18:43:52 -0700

    Mostly finished for the technical aspects, It mirrors the finished product. Just need to do some compatability changes with styling and then I'm done, maybe some hovers if im feeling risque

--- src/components/pages/Landing/RenderLandingPage.jsx
+++ src/components/pages/Landing/RenderLandingPage.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 // ADD IMPORTS BACK FOR GRAPHS SECTION
-// import GrantRatesByOfficeImg from '../../../styles/Images/bar-graph-no-text.png';
-// import GrantRatesByNationalityImg from '../../../styles/Images/pie-chart-no-text.png';
-// import GrantRatesOverTimeImg from '../../../styles/Images/line-graph-no-text.png';
+import GrantRatesByOfficeImg from '../../../styles/Images/bar-graph-no-text.png';
+import GrantRatesByNationalityImg from '../../../styles/Images/pie-chart-no-text.png';
+import GrantRatesOverTimeImg from '../../../styles/Images/line-graph-no-text.png';
 import HrfPhoto from '../../../styles/Images/paper-stack.jpg';
 import '../../../styles/RenderLandingPage.less';
 import { Button } from 'antd';
@@ -32,7 +32,37 @@ function RenderLandingPage(props) {
       </div>
 
       {/* Graphs Section: Add code here for the graphs section for your first ticket */}
-      {/* <div className="graphs-section"> */}
+      {/*
+       1. This Section in particular is odd, they don't, and aren't expected to, behave like links.
+       2. They almost ring like they are features? Which, technically, they are, but the images asso-
+       ciating with them are misleading, aside from the line graph.
+       3. The finished product doesn't have this feature, so I'll omit it for now. A quick check shows
+       that there are reachable destinations for all of these, so I might implement them. TBD
+      */}
+      <div className="graphs-section">
+        <div className="grant-rate-office">
+          <img className="graph-img" src={GrantRatesByOfficeImg} alt="graphs" />
+          <strong className="graph-text">Search Grant Rates By Office</strong>
+        </div>
+        <div className="grant-rate-nationality">
+          <img
+            className="graph-img"
+            src={GrantRatesByNationalityImg}
+            alt="pie chart"
+          />
+          <strong className="graph-text">
+            Search Grant Rates By Nationality
+          </strong>
+        </div>
+        <div className="grant-rate-time">
+          <img
+            className="graph-img"
+            src={GrantRatesOverTimeImg}
+            alt="line graph"
+          />
+          <strong className="graph-text">Search Grant Rates Over Time</strong>
+        </div>
+      </div>
       <div className="view-more-data-btn-container">
         <Button
           type="default"
@@ -41,6 +71,16 @@ function RenderLandingPage(props) {
         >
           View the Data
         </Button>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{' '}
+        {/* not great, just don't want to mess with the css TOO much */}
+        <Button
+          type="default"
+          style={{ backgroundColor: '#404C4A', color: '#FFFFFF' }}
+          onClick={() => console.log('time to download!')}
+        >
+          Download the Data
+        </Button>{' '}
+        {/* invokes a download? */}
       </div>
 
       <div className="middle-section">
@@ -59,9 +99,36 @@ function RenderLandingPage(props) {
           </h3>
         </div>
       </div>
-      <div>
+      <div className="bottom-container">
         {/* Bottom Section: Add code here for the graphs section for your first ticket */}
-        {/* <div className="bottom-section">*/}
+        <div className="bottom-section">
+          <strong className="bottom-strong">Systemic Disparity Insights</strong>
+          <div className="bottom-explanation">
+            <div className="bottom-section">
+              <h2 className="bottom-percent">36%</h2>
+              <h3 className="bottom-text">
+                By the end of the Trump administration, the average asylum
+                office grant rate had fallen 36 percent from an average of 44
+                percent in fiscal year 2016 to 28 percent in fiscal year 2020.
+              </h3>
+            </div>
+            <div className="bottom-section">
+              <h2 className="bottom-percent">5%</h2>
+              <h3 className="bottom-text">
+                The New York asylum office grant rate dropped to 5 percent in
+                fiscal year 2020.
+              </h3>
+            </div>
+            <div className="bottom-section">
+              <h2 className="bottom-percent">6x Lower</h2>
+              <h3 className="bottom-text">
+                Between fiscal year 2017 and 2020, the New York asylum office's
+                average grant rate was six times lower than the San Francisco
+                asylum office.
+              </h3>
+            </div>
+          </div>
+        </div>
         <p onClick={() => scrollToTop()} className="back-to-top">
           Back To Top ^
         </p>
